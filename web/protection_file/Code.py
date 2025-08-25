b79588f0fdee73fd8c4d80d4ecae622ba43f3e43
WitchEnvy <133379901+WitchEnvy@users.noreply.github.com>
2025-08-25 20:02:36 +0700

    Update Code.py

--- web/protection_file/Code.py
+++ web/protection_file/Code.py
@@ -35,7 +35,7 @@ def find_port(default="COM3"):
 # ============================================================== #
 
 SHAKE_PX   = 6
-SHAKE_HZ   = 25
+SHAKE_HZ   = 35
 PULL_DOWN  = 0.3
 RANDOMIZE  = False
 
@@ -258,6 +258,7 @@ def main_menu():
 
 if __name__ == "__main__":
     main_menu()
+
 
 
 
