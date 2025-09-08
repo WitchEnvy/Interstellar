047741649997d46bd7cdcb71e04b79acfc9376be
WitchEnvy <133379901+WitchEnvy@users.noreply.github.com>
2025-09-08 18:11:03 +0700

    Update Code.py

--- web/protection_file/Code.py
+++ web/protection_file/Code.py
@@ -129,7 +129,7 @@ def aim_loop(ser):
         screen_h = user32.GetSystemMetrics(1)
         monitor = {
             "left": (screen_w // 2) - SCAN_WIDTH // 2,
-            "top": (screen_h // 2) - SCAN_HEIGHT // 1,
+            "top": (screen_h // 2) - SCAN_HEIGHT // 2,
             "width": SCAN_WIDTH,
             "height": SCAN_HEIGHT
         }
@@ -258,6 +258,7 @@ def main_menu():
 
 if __name__ == "__main__":
     main_menu()
+
 
 
 
